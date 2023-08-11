bbb9bbea9229abb1fcccaaa3142a54917890b2ff
LaidBenglia <73167661+LaidBengli@users.noreply.github.com>
2023-08-11 14:48:40 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+/* Document*/
+
+document.getElementById('text').innerHTML = "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded");
+    document.getElementById('text').innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically."
+});
+
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+
+
